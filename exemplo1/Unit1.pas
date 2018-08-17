0df582662590c5ede8fbf673b8588777ce74b765
Adriano Santos <asrsantos@gmail.com>
2018-08-17 11:43:23 -0300

    Exemplo de uso do GridSystem

---
+++ exemplo1/Unit1.pas
@@ -0,0 +1,61 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Objects, System.Generics.Collections, GridSystem, FMX.Layouts,
+  FMX.Controls.Presentation;
+
+type
+  TForm1 = class(TForm)
+    GSContainer1: TGSContainer;
+    ToolBar1: TToolBar;
+    VertScrollBox1: TVertScrollBox;
+    GSContainer2: TGSContainer;
+    GSRow1: TGSRow;
+    GSCol1: TGSCol;
+    Rectangle1: TRectangle;
+    RoundRect1: TRoundRect;
+    Label1: TLabel;
+    Label2: TLabel;
+    Layout1: TLayout;
+    GSCol2: TGSCol;
+    RoundRect2: TRoundRect;
+    Rectangle2: TRectangle;
+    Layout2: TLayout;
+    Label3: TLabel;
+    Label4: TLabel;
+    GSCol3: TGSCol;
+    RoundRect3: TRoundRect;
+    Rectangle3: TRectangle;
+    Layout3: TLayout;
+    Label5: TLabel;
+    Label6: TLabel;
+    GSCol4: TGSCol;
+    RoundRect4: TRoundRect;
+    Rectangle4: TRectangle;
+    Layout4: TLayout;
+    Label7: TLabel;
+    Label8: TLabel;
+    GSCol5: TGSCol;
+    RoundRect5: TRoundRect;
+    Rectangle5: TRectangle;
+    Layout5: TLayout;
+    Label9: TLabel;
+    Label10: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
---
+++ exemplo2/Unit1.pas
@@ -0,0 +1,55 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls, FMX.Objects,
+  System.Generics.Collections, GridSystem, FMX.Layouts;
+
+type
+  TForm1 = class(TForm)
+    GSContainer1: TGSContainer;
+    GSRow1: TGSRow;
+    GSCol1: TGSCol;
+    Circle1: TCircle;
+    Label1: TLabel;
+    GSCol2: TGSCol;
+    Circle2: TCircle;
+    Label2: TLabel;
+    GSCol3: TGSCol;
+    Circle3: TCircle;
+    Label3: TLabel;
+    GSCol4: TGSCol;
+    Circle4: TCircle;
+    Label4: TLabel;
+    GSCol5: TGSCol;
+    Circle5: TCircle;
+    Label5: TLabel;
+    GSCol6: TGSCol;
+    Circle6: TCircle;
+    Label6: TLabel;
+    GSCol7: TGSCol;
+    Circle7: TCircle;
+    Label7: TLabel;
+    GSCol8: TGSCol;
+    Circle8: TCircle;
+    Label8: TLabel;
+    GSCol9: TGSCol;
+    Circle9: TCircle;
+    Label9: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
